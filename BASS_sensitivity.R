5ccdc265fa93f1f92d3b594fa73adda08ebc474c
Haochen Ye <39839938+yhaochen@users.noreply.github.com>
2022-11-15 23:53:41 -0500

    Upload scripts and data

---
+++ BASS_sensitivity.R
@@ -0,0 +1,47 @@
+#BASS method
+rm(list = ls())
+graphics.off()
+library(BASS)
+folder <- "/storage/work/h/hxy46/Sensitivity/Iman_test"
+setwd(folder)
+
+#------------------------------Parameters that users need to set
+seed <- 1
+size <- 10000
+# in which metric? "Hazard" or "Risk"
+m <- "Hazard" #"Risk"
+# 3 important parameters in MCMC setting:
+# total MCMC length
+nmcmc <- 1000000
+# BASS records the results every 'thin' iterations
+thin <- 10000
+# burn-in period, only record the results after this many iterations
+nburn <- 10000
+#------------------------------------
+
+set.seed(seed)
+#Load data
+load("BASS_param_set.RData")
+selected_index <- sample(1:30000, size, replace=F)
+
+# In the dataframe, the 9th parameter is hazard, the 10th parameter is risk.
+# Hazard analysis needs 6 parameters, risk analysis needs 8 parameters.
+if (m == "Hazard"){
+  m_index <- 9
+  npara <- 6
+}
+if (m == "Risk"){
+  m_index <- 10
+  npara <- 8
+}
+Metric <- BASS_param_set[selected_index, m_index]
+Param <- BASS_param_set[selected_index, c(1:npara)]
+# Note we allow the max interactions to be 3 and the max basis function numbers to be 200.
+# Users may also change these settings.
+BASS_model <- bass(Param, Metric, nmcmc = nmcmc, nburn = nburn, thin = thin, maxInt = 3, maxBasis = 200)
+S <- sobol(BASS_model, verbose = TRUE)
+save(S,file = paste("sensitivity_", m, "_", seed, "_", size, sep=""))
+
+
+
+
---
+++ wheel_plot.R
@@ -0,0 +1,128 @@
+# This script plots the radial plots for the sensitivity analysis.
+# This script is modified based on "12-radial_plot_risk.R"
+rm(list = ls())
+graphics.off()
+library(plotrix)
+
+folder <- "/storage/work/h/hxy46/Sensitivity/Iman_test"
+setwd(folder)
+
+#------------------------------ parameters to change
+m <- "Hazard" # or "Risk"
+seed <- 1
+size <- 10000
+# Below are the two indices that represent the samples in the sensitivity analysis results
+#    that you treat as converged. (For example, S records 90 samples after burn-in period, 
+#    you may take the mean of sample 80 to sample 90 as the true sensitivity index)
+ind_start <- 80
+ind_end <- 90
+#---------------------------------
+
+# Load the sensitivity analysis results (may need to change these parameters based on your results)
+load(paste("sensitivity_", m, "_", seed, "_", size, sep=""))
+
+# Parameters and names
+if (m == "Hazard"){
+  n_params <- 6
+  names <- c("Q","Z","W",
+             "n_ch","n_fp","DEM")
+}
+if (m == "Risk"){
+  n_params <- 8
+  names <- c("Q","Z","W","n_ch","n_fp","DEM","V","X")
+}
+
+# A threshold that determines which indices are statistical significant
+#   in the order of: first-order, total-order, second-order indices
+thres <- c(0.05,0.05,0.05)
+
+# Settings for the radial plot
+cent_x=0
+cent_y=0.2
+radi=0.6
+alph=360/8 # or use n_params
+
+# A function to create an upper diagonal matrix for 2nd-order indices
+upper.diag <- function(x){
+  m<-(-1+sqrt(1+8*length(x)))/2+1
+  X<-lower.tri(matrix(NA,m,m),diag=FALSE)
+  X[X==TRUE]<-x
+  X
+}
+
+# Each index is the mean of the last saved MCMC iterations (you may choose how many samples to use)
+# Total-order indices:
+st <- colMeans(S$T[c(ind_start:ind_end), ])
+    
+# First-order indices:
+s1 <- colMeans(S$S[c(ind_start:ind_end),c(1:6)])
+    
+# second-order indices:
+s2 <- colMeans(S$S[c(ind_start:ind_end),c(7:21)])
+s2 <- upper.diag(s2)
+    
+pdfname <- paste("radial_plot_Hazard.pdf",sep="")
+pdf(pdfname, width =3.94, height =3.94)
+    
+par(cex=0.5,mai=c(0.1,0.1,0.1,0.1))
+plot(c(-1,1),c(-1,1),bty="n",xlab="",ylab="",xaxt="n",yaxt="n",type="n")
+draw.circle(0,.2,0.5,border = NA,col="gray90")
+    
+for(j in 1:(n_params)){
+  i=j-1
+  cosa=cospi(alph*i/180)
+  sina=sinpi(alph*i/180)
+  text(cent_x+cosa*(radi+radi*.15),cent_y+sina*(radi+radi*.15),
+    names[j],srt=0,cex=1,col="darkgreen")
+  myX=cent_x+cosa*(radi-0.2*radi)
+  myY=cent_y+sina*(radi-0.2*radi)
+  if (j < n_params){
+    for (z in (j+1):n_params){ #Second-order interactions 
+      if (s2[z,j]>=thres[3]){
+        g <- z-1
+        cosaa=cospi(alph*g/180)
+        sinaa=sinpi(alph*g/180)
+        EndX=cent_x+cosaa*(radi-0.2*radi)
+        EndY=cent_y+sinaa*(radi-0.2*radi)
+        lines(c(myX,EndX),c(myY,EndY),col='darkblue',
+                  lwd=qunif(s2[z,j]/max(s2),0,5))
+      }
+    }
+  }
+      
+  if(st[j]>=thres[2]){ #Total-order nodes 
+    draw.circle(cent_x+cosa*(radi-0.2*radi),cent_y+sina*(radi-0.2*radi),
+                radius = qunif(st[j]/max(st),0.03,0.1),col="black")
+  }
+      
+  if(s1[j]>=thres[1]){ #First-order nodes 
+    draw.circle(cent_x+cosa*(radi-0.2*radi),cent_y+sina*(radi-0.2*radi),
+                radius = qunif(s1[j]/max(s1),0.01,0.08),
+                col=rgb(1, 102/255, 102/255,1),border = NA)
+  }
+}
+    
+# Plot the box below the plot 
+x1=0.3
+y1=0
+draw.circle(x1+-0.9,y1+-0.97,0.08,border = NA,col=rgb(1, 102/255, 102/255,1))
+draw.circle(x1+-0.7,y1+-0.97,0.01,border = NA,col=rgb(1, 102/255, 102/255,1))
+text(x1+-0.9,y1+-0.83,paste(round(100*max(s1)),'%',sep=""))
+text(x1+-0.7,y1+-0.83,paste(round(100*max(min(s1),thres[1])),'%',sep=""))
+text(x1+-0.8,y1+-0.75,'First-order')
+    
+draw.circle(x1+-0.4,y1+-0.97,0.1,col="black")
+draw.circle(x1+-0.2,y1+-0.97,0.03,col="black")
+text(x1+-0.4,y1+-0.83,paste(round(100*max(st)),'%',sep=""))
+text(x1+-0.2,y1+-0.83,paste(round(100*max(min(s1),thres[2])),'%',sep=""))
+text(x1+-0.3,y1+-0.75,'Total-order')
+    
+lines(c(x1+0.1,x1+0.2),c(y1+-0.97,y1+-0.97),lwd=5,col="darkblue")
+text(x1+0.15,y1+-0.83,paste(round(100*max(s2,na.rm=T)),'%',sep=""))
+text(x1+0.15,y1+-0.75,'Second-order')
+    
+#title of used samples and seed
+x2=0
+y2=1
+text(x2,y2,"")
+dev.off()
